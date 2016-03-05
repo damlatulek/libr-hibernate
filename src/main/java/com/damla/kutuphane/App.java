31f42ad42d7431b140814e3edcaed535f3ebf9a0
damlatulek <vektorel@LAB3-111>
2016-03-05 13:05:23 +0200

    hibernate2

--- src/main/java/com/damla/kutuphane/App.java
+++ src/main/java/com/damla/kutuphane/App.java
@@ -3,23 +3,30 @@ package com.damla.kutuphane;
 import com.damla.kutuphane.entity.Kisi;
 import com.damla.kutuphane.framework.DBException;
 import com.damla.kutuphane.service.KisiService;
+import java.util.Date;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main(String[] args) {
+        KisiService ks = new KisiService();
         try {
-            System.out.println( "Hello World!" );
+            System.out.println("Hello World!");
+
             
-            KisiService ks = new KisiService();
-            ks.save(new Kisi());
+            ks.save(new Kisi(Long.MIN_VALUE, "damla", "tülek", new Date(), "mersin", "damlatulek@gmail.com", false));
         } catch (DBException ex) {
-           JOptionPane.showMessageDialog(null, ex.getMessage());
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+        List<Kisi> kisiListe = ks.getAll();
+
+        for (Kisi kisi : kisiListe) {
+            System.out.println("sdfds");
         }
     }
 }
